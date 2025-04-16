9bc009ddcb02b50255be220421bc3e538f1f0d15
kolosovpetro <kolosovp94@gmail.com>
2025-04-16 20:53:23 +0200

    work in progress

--- GoldbachPairs.Run/Program.cs
+++ GoldbachPairs.Run/Program.cs
@@ -12,5 +12,19 @@ internal static class Program
         var goldbachPairsMin = GoldbachHelper.GetMinimalGoldbachPairs(50);
 
         var count = GoldbachHelper.CountMinPairs(1000000, 3);
+
+        var list = new List<int>();
+
+        for (int i = 5; i <= 100; i++)
+        {
+            var p = GoldbachHelper.CountMinPairs(i, 3) + 1;
+            
+            list.Add(p);
+        }
+
+        foreach (var i in list)
+        {
+            Console.Write($"{i}, ");
+        }
     }
 }
