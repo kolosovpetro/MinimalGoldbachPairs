32c080651dec9cbf7875bb147a28c1386df6f9cb
kolosovpetro <kolosovp94@gmail.com>
2025-04-22 01:17:44 +0200

    work in progress

--- GoldbachPairs.Run/Program.cs
+++ GoldbachPairs.Run/Program.cs
@@ -8,18 +8,23 @@ internal static class Program
     public static void Main()
     {
         var sieve = GoldbachHelper.Primes.SieveOfEratosthenes;
-        var pjSequence = GoldbachHelper
-            .GeneratePjSequence(2000, 5)
-            .Except(GoldbachHelper.GeneratePjSequence(2000, 7))
-            .ToList();
+        var pjSequence =
+            GoldbachHelper
+                .GeneratePjSequence(2000, 5)
+                .Except(GoldbachHelper.GeneratePjSequence(2000, 7))
+                .ToList();
 
-        // for (int i = 0; i < pjSequence.Count-1; i++)
-        // {
-        //     var diff = pjSequence[i + 1] - pjSequence[i];
-        //
-        //     Console.Write($"{diff}, ");
-        // }
+        for (int i = 1; i < 7; i++)
+        {
+            var power = (int)Math.Pow(10, i);
+            var min5 = GoldbachHelper.GeneratePjSequence(power, 5).Count;
+            var min7 = GoldbachHelper.GeneratePjSequence(power, 7).Count;
 
-         pjSequence.ForEach(x => Console.Write($"{x}, "));
+            var result = min5 - min7;
+
+            Console.Write($"power: {power}: {result}, ");
+        }
+
+        // pjSequence.ForEach(x => Console.Write($"{x}, "));
     }
 }
